3f610e07e8c6d103c915a48305239df42c524204
Alisson <damascenoalisson@gmail.com>
2021-04-07 18:18:38 -0300

    Fix wrong groupby clause

--- queries/query2.sql
+++ queries/query2.sql
@@ -6,7 +6,7 @@ FROM   shops sp
                ON sl.shop_id = sp.shop_id
        INNER JOIN items it
                ON it.item_id = sl.item_id
-GROUP  BY it.item_name
+GROUP  BY sp.shop_name
 HAVING "itens a venda" > 5000
 ORDER  BY "itens a venda" DESC,
           Sum(sl.item_cnt_day)
